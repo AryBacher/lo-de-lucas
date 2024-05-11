35ed2efcef4b8567cf464f2eed93fc46ae0232c0
tomaslami <tomaslamiguralnik@gmail.com>
2024-05-11 13:57:27 -0300

    feat: probando error, (hay que hacer optimizacion del codigo)

--- src/app/[comida_Id]/page.tsx
+++ src/app/[comida_Id]/page.tsx
@@ -1,41 +1,36 @@
 "use client"
-import { useParams } from 'next/navigation'
-import Comida from '../../components/helpers/Comida'
+import { useParams } from 'next/navigation';
+import Comida from '../../components/helpers/Comida';
 
 type ComidaData = {
-  comida_Id: string | string[]
-  description: string
-  price: number
-}
+  description: string;
+  price: number;
+};
 
 const comidasData: Record<string, ComidaData> = {
   milanesas: {
-    comida_Id: "milanesas",
     description: "Milanesas",
     price: 10
   },
   pastas: {
-    comida_Id: "pastas",
     description: "Pastas",
     price: 12
   },
   postres: {
-    comida_Id: "postres",
     description: "Postres",
     price: 5
   }
-}
+};
 
 const page = () => {
-  const { comida_Id } = useParams()
-  const firstComidaId = Array.isArray(comida_Id) ? comida_Id[0] : comida_Id;
-  const comida = firstComidaId ? comidasData[firstComidaId] : undefined;
-  const title = typeof firstComidaId === 'string' ? firstComidaId : '';
+  const { comidaId } = useParams();
+  const comidaData = comidasData[comidaId as string];
+
   return (
     <div className='w-full h-screen'>
-      {comida && <Comida title={title} description={comida.description} price={comida.price} />}
+      {comidaData && <Comida title={comidaId as string} description={comidaData.description} price={comidaData.price} />}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
