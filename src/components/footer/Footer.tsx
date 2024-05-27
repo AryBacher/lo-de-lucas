87e4405b93903525a7680a3fefac27b31dc1604b
tomaslami <tomaslamiguralnik@gmail.com>
2024-05-26 22:30:50 -0300

    feat: footer texto arreglado

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -75,7 +75,7 @@ const Footer = () => {
           ))}
         </ul >
       </div>
-      <label className={`${poppins.className} text-white font-semibold mt-5`}>Pedí al 0348 443-0751 y vení a retirar al local</label>
+      <label className={`${poppins.className} text-white font-semibold mt-5 text-center`}>Pedí al 0348 443-0751 y vení a retirar al local</label>
       <div className="w-[85%] md:w-[47%] h-max flex items-center justify-center border-t-[1px] border-white/20 flex-col pb-4">
         <small className={`${poppins.className} text-center text-white text-sm font-bold mt-2 `}>
           © 2024 Lo De Lucas. Todos los derechos reservados.
